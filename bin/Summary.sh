4dc60c76b9c3da18de05fbaa0c4d17acdecb4621
Derek <derek_sarovich@hotmail.com>
2024-03-01 13:11:16 +1000

    Update Summary.sh

--- bin/Summary.sh
+++ bin/Summary.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-##This script is run as part of the SPANDx pipeline as of version 3.1
 
-PBS_O_WORKDIR=$1
-ref=$2
+ref=$1
+baseDir=$2
+
 SAMTOOLS=$3
 
 ##input sample names
 
-cd $PBS_O_WORKDIR
+#cd $PBS_O_WORKDIR
 
-sequences_tmp=(`find $PBS_O_WORKDIR/*_1_sequence.fastq.gz -printf "%f "`)
-sequences=("${sequences_tmp[@]/_1_sequence.fastq.gz/}")
+sequences_tmp=(`find $baseDir/Outputs/Variants/VCFs/*.PASS.snps.indels.mixed.vcf -printf "%f "`)
+sequences=("${sequences_tmp[@]/.PASS.snps.indels.mixed.vcf/}")
 n=${#sequences[@]}
 
 TAB="$(printf '\t')"
@@ -28,20 +28,25 @@ _EOF_
 
 for (( i=0; i<n; i++ )); do
   
-  SNP_PASS_count=$(cat Outputs/SNPs_indels_PASS/${sequences[$i]}.snps.PASS.vcf | grep -v '#' | wc -l)
-  SNP_FAIL_count=$(cat Outputs/SNPs_indels_FAIL/${sequences[$i]}.snps.FAIL.vcf | grep -v '#' | wc -l)
-  indel_PASS_count=$(cat Outputs/SNPs_indels_PASS/${sequences[$i]}.indels.PASS.vcf | grep -v '#' | wc -l)
-  indel_FAIL_count=$(cat Outputs/SNPs_indels_FAIL/${sequences[$i]}.indels.FAIL.vcf | grep -v '#' | wc -l)
+  SNP_PASS_count=$(cat $baseDir/Outputs/Variants/VCFs/${sequences[$i]}.snps.PASS.vcf | grep -v '#' | wc -l)
+  #SNP_FAIL_count=$(cat $baseDir/Outputs/SNPs_indels_FAIL/${sequences[$i]}.snps.FAIL.vcf | grep -v '#' | wc -l)
+  indel_PASS_count=$(cat $baseDir/Outputs/SNPs_indels_PASS/${sequences[$i]}.indels.PASS.vcf | grep -v '#' | wc -l)
+  #indel_FAIL_count=$(cat $baseDir/Outputs/SNPs_indels_FAIL/${sequences[$i]}.indels.FAIL.vcf | grep -v '#' | wc -l)
   Avg_cov=$(cat ${sequences[$i]}/unique/${sequences[$i]}.sample_summary | awk '{print $3}' | head -n2 | tail -n1)
-  Mapped_reads=$($SAMTOOLS idxstats ${sequences[$i]}/unique/${sequences[$i]}.realigned.bam |  awk '{ total += $3 } END {print total }')
+  Mapped_reads=$(samtools idxstats $baseDir/Outputs/bams/${sequences[$i]}.dedup.bam |  awk '{ total += $3 } END {print total }')
   
   echo -e "${sequences[$i]}\t$SNP_PASS_count\t$SNP_FAIL_count\t$indel_PASS_count\t$indel_FAIL_count\t$Avg_cov\t$Mapped_reads" > ${sequences[$i]}.summary
   
-done  
+done
   
 cat SNP_summary_header ${sequences[@]/%/.summary} > Outputs/Single_sample_summary.txt
 
 rm SNP_summary_header
 rm ${sequences[@]/%/.summary} 
+
+
+
+QC_metrics_summary.tsv
+
 
 exit 0
